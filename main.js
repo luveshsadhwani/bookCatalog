e98871b4b4e321387a567da3ee9562530f3b561d
Luvesh L Sadhwani <luveshsadhwani@gmail.com>
2020-12-02 19:28:56 +0800

    Add trash can icon and animation

--- main.js
+++ main.js
@@ -10,12 +10,20 @@ const enterBtn = document.getElementById('myButton');
 
 enterBtn.addEventListener("click",getInputVal);
 
-function getInputVal() {
+function getInputVal(event) {
+    // Prevent page from refreshing when form is submitted
+    event.preventDefault();
+    // Creating a new row for the table
     let addBook = document.createElement('tr');
-    addBook.innerHTML = "<td>" + bookTitle.value + "</td>" + "<td>" + bookAuthor.value + "</td>" + "<td>" + bookGenre.value + "</td>" + "<td>" + bookPurchase.value + "</td>" + "<td>" + bookIsbn.value + "</td>";
+    addBook.innerHTML = "<td>" + bookTitle.value + "</td>" + "<td>" + bookAuthor.value + "</td>" + "<td>" + bookGenre.value + "</td>" + "<td>" + bookPurchase.value + "</td>" + "<td>" + bookIsbn.value + "</td>" + '<td id=del><i class="fas fa-trash-alt"></i></td>';
     bookList.appendChild(addBook);
+    addBook.style.animation = 'fadein 1s'; // adding Anmiation
+    // Resetting the values in the form
     bookTitle.value = "";
     bookAuthor.value = "";
     bookPurchase.value = "";
     bookIsbn.value = "";
 }
+
+
+
